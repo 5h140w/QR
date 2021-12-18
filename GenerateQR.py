c3ad0a826b8d5cf4092863802d66fa99bc2e309e
5h140w <5h140w23@gmail.com>
2021-12-18 16:46:57 +0100

    v2

--- GenerateQR.py
+++ GenerateQR.py
@@ -1,9 +1,24 @@
 import qrcode
 
-#standard qrcode
+#qrcodev1 (only link)
 ##Function: 
-def easyWay(link):
+def v1(link):
     img= qrcode.make(link)
     img.save("xx.png")
 
-easyWay("facebook.com")
+v1("facebook.com")
+
+#qrcodev2(link + colors)
+def v2(link,colors):
+    qr=qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=12,
+        border=2
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill=colors[0],back_color=colors[1])
+    img.save("xx1.png")
+
+v2("wwww",["black","white"])
