1f62db2e123d217ecc32b536bf3cf4fc2f0e3518
5h140w <5h140w23@gmail.com>
2021-12-19 11:09:44 +0100

    v1

--- GenerateQR.py
+++ GenerateQR.py
@@ -1,35 +1,22 @@
-import qrcode
-import PIL
-from qrcode import constants
+#isFacebook : verify if it's a facebook link
+def isFacebook(string):
+    if ("facebook" in string.lower() or "fb" in string.lower()):
+        return True
+    else : 
+        return False
 
-#qrcodev1 (only link)
-##Function: 
-def v1(link):
-    img= qrcode.make(link)
-    img.save("xx.png")
+#isTwitter : verify if it's a twitter link
+def isTwitter(string):
+    if("twitter" in string.lower()):
+        return True
+    else:
+        return False
 
-v1("facebook.com")
-
-#qrcodev2(link + colors)
-def v2(link,colors):
-    qr=qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=12,
-        border=2
-    )
-    qr.add_data(link)
-    qr.make(fit=True)
-    img = qr.make_image(fill=colors[0],back_color=colors[1])
-    print(type(img))
-    img.save("xx1.png")
-
-v2("wwww",["black","white"])
+#isGithub : verify if it's a github link
+def isGithub(string):
+    if("github" in string.lower()):
+        return True
+    else:
+        return False
 
 
-##qrcode3(link+color+logo)
-def v3(link,color,logo):
-    qr= qrcode.QRCode(version=2,error_correction=constants.ERROR_CORRECT_M,box_size=12,border=5)
-    qr.add_data(link)
-    qr.make(fit=True)
-    img = qr.make_image()
