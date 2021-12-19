5f639fb31105e233056839f4ecb95b834e9e46df
5h140w <5h140w23@gmail.com>
2021-12-19 12:57:09 +0100

    v1

--- Verification.py
+++ Verification.py
@@ -1,20 +1,16 @@
 #isFacebook : verify if it's a facebook link
 def isFacebook(string):
-    if ("facebook" in string.lower() or "fb" in string.lower()):
-        return True
-    else : 
-        return False
+    return "facebook" in string.lower() or "fb" in string.lower()
 
 #isTwitter : verify if it's a twitter link
 def isTwitter(string):
-    if("twitter" in string.lower()):
-        return True
-    else:
-        return False
+    return "twitter" in string.lower()
+
 
 #isGithub : verify if it's a github link
 def isGithub(string):
-    if("github" in string.lower()):
-        return True
-    else:
-        return False
+    return "github" in string.lower()
+
+#isInstagram: verify if it's an instagram link
+def isInstagram(string):
+    return "instagram" in string.lower()
